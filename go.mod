f4bf6ec2edfd048fdc8674102f1abc2e20b0fe2d
iambighead <iambighead168@gmail.com>
2022-11-24 17:12:20 +0800

    initial working sftp client test code

--- go.mod
+++ go.mod
@@ -2,12 +2,17 @@ module github.com/iambighead/ugoku
 
 go 1.18
 
-require github.com/iambighead/goutils v0.0.2
+require (
+	github.com/iambighead/goutils v0.0.2
+	github.com/pkg/sftp v1.13.5
+	golang.org/x/crypto v0.3.0
+)
 
 require (
+	github.com/kr/fs v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/rs/zerolog v1.28.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- ugoku.go
+++ ugoku.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/iambighead/goutils/logger"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 )
 
 const VERSION = "v0.0.1"
@@ -21,4 +24,73 @@ func main() {
 	fmt.Printf("ugoku %s\n\n", VERSION)
 
 	main_logger.Info("app started")
+
+	// var hostKey ssh.PublicKey
+	// An SSH client is represented with a ClientConn.
+	//
+	// To authenticate with the remote server you must pass at least one
+	// implementation of AuthMethod via the Auth field in ClientConfig,
+	// and provide a HostKeyCallback.
+	config := &ssh.ClientConfig{
+		User: "st",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("P@55w0rd"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	ssh_client, err := ssh.Dial("tcp", "192.168.55.162:22", config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer ssh_client.Close()
+
+	// // Each ClientConn can support multiple interactive sessions,
+	// // represented by a Session.
+	// session, err := client.NewSession()
+	// if err != nil {
+	// 	log.Fatal("Failed to create session: ", err)
+	// }
+	// defer session.Close()
+
+	// // Once a Session is created, you can execute a single command on
+	// // the remote side using the Run method.
+	// var b bytes.Buffer
+	// session.Stdout = &b
+	// if err := session.Run("/usr/bin/whoami"); err != nil {
+	// 	log.Fatal("Failed to run: " + err.Error())
+	// }
+	// fmt.Println(b.String())
+
+	// open an SFTP session over an existing ssh connection.
+	client, err := sftp.NewClient(ssh_client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// walk a directory
+	w := client.Walk("/home/st")
+	for w.Step() {
+		if w.Err() != nil {
+			continue
+		}
+		log.Println(w.Path())
+	}
+
+	// leave your mark
+	f, err := client.Create("hello.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte("Hello world!")); err != nil {
+		log.Fatal(err)
+	}
+	f.Close()
+
+	// check it's there
+	fi, err := client.Lstat("hello.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(fi)
 }
