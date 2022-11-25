78da441be16cf7da519a81cefa22f1102a67d6b6
iambighead <iambighead168@gmail.com>
2022-11-25 12:11:46 +0800

    downloader - working download life cycle: scan dowload delete

--- downloader.go
+++ downloader.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/iambighead/goutils/logger"
@@ -16,9 +18,10 @@ import (
 // --------------------------------
 
 type Downloader interface {
-	start()
-	stop()
-	scan()
+	Start()
+	Stop()
+	init()
+	scan() []string
 	download()
 }
 
@@ -30,18 +33,40 @@ type MyDownloader struct {
 	ssh_client  *ssh.Client
 }
 
-func (dler *MyDownloader) Download(filepath string, c chan int) {
+func (dler *MyDownloader) scan() []string {
+	var filelist []string
+	// walk a directory
+	w := dler.sftp_client.Walk(dler.SourcePath)
+	for w.Step() {
+		if w.Err() != nil {
+			continue
+		}
+		if !w.Stat().IsDir() {
+			filelist = append(filelist, w.Path())
+		}
+		dler.logger.Debug(fmt.Sprintf("path=%s, isDir=%t", w.Path(), w.Stat().IsDir()))
+	}
 
-	dler.logger.Info(fmt.Sprintf("Downloading file %s", filepath))
+	return filelist
+}
+
+func (dler *MyDownloader) download(file_to_download string) {
+
+	output_file := filepath.Join(dler.TargetPath, file_to_download)
+	dler.logger.Debug(fmt.Sprintf("Downloading file %s to %s", file_to_download, output_file))
+
+	output_parent_folder := filepath.Dir(output_file)
+	os.MkdirAll(output_parent_folder, fs.ModeDir)
+	dler.logger.Debug(fmt.Sprintf("created output folder %s", output_parent_folder))
 
 	start_time := time.Now().UnixMilli()
-	source, err := dler.sftp_client.OpenFile(filepath, os.O_RDONLY)
+	source, err := dler.sftp_client.OpenFile(file_to_download, os.O_RDONLY)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer source.Close()
 
-	destination, err := os.Create(fmt.Sprintf("local-%s", filepath))
+	destination, err := os.Create(output_file)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,9 +79,12 @@ func (dler *MyDownloader) Download(filepath string, c chan int) {
 	end_time := time.Now().UnixMilli()
 
 	time_taken := end_time - start_time
-	dler.logger.Info(fmt.Sprintf("downloaded %d bytes in %ds, %.1f mbps", nBytes, time_taken, float64(nBytes/1000*8/time_taken)))
+	dler.logger.Info(fmt.Sprintf("downloaded %s with %d bytes in %ds, %.1f mbps", file_to_download, nBytes, time_taken, float64(nBytes/1000*8/time_taken)))
 
-	c <- 1
+	err = dler.sftp_client.Remove(file_to_download)
+	if err != nil {
+		dler.logger.Error(fmt.Sprintf("failed to remove remote file: %s: %x", file_to_download, err))
+	}
 }
 
 func (dler *MyDownloader) Stop() {
@@ -65,7 +93,7 @@ func (dler *MyDownloader) Stop() {
 	dler.ssh_client.Close()
 }
 
-func (dler *MyDownloader) Init() {
+func (dler *MyDownloader) init() {
 	dler.started = false
 	dler.logger = logger.NewLogger(fmt.Sprintf("downloader[%s]", dler.Name))
 
@@ -96,14 +124,26 @@ func (dler *MyDownloader) Init() {
 }
 
 func (dler *MyDownloader) Start() {
-	dler.Init()
+	dler.init()
 	dler.started = true
-
-	c := make(chan int)
-
-	go dler.Download("amz-deno", c)
-
-	<-c
+	sleep_time := 1
+	for {
+		filelist := dler.scan()
+		files_found := len(filelist)
+		dler.logger.Debug(fmt.Sprintf("scan returned %d files", files_found))
+		if files_found == 0 {
+			if sleep_time < 16 {
+				sleep_time = sleep_time * 2
+			}
+		} else {
+			sleep_time = 1
+			for _, file_to_download := range filelist {
+				dler.download(file_to_download)
+			}
+		}
+		dler.logger.Debug(fmt.Sprintf("sleep for %ds", sleep_time))
+		time.Sleep(time.Duration(sleep_time) * time.Second)
+	}
 }
 
 func startDownloaders(master_config config.MasterConfig) {
