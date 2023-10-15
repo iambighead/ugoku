5ddaaf139338142b0bac84350de262f1a203aae1
iambighead <iambighead168@gmail.com>
2023-10-15 14:00:01 +0800

    merge cli and service into single service

--- cmd/service/ugoku.go
+++ cmd/service/ugoku.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/iambighead/goutils/logger"
 	"github.com/iambighead/ugoku/downloader"
@@ -26,6 +27,76 @@ func startDownloaders(master_config config.MasterConfig) {
 	downloader_started := 0
 	for _, downloader_config := range master_config.Downloaders {
 		if downloader_config.Enabled {
+			downloader.NewOneTimeDownloader(downloader_config, master_config.General.TempFolder)
+			downloader_started++
+		}
+	}
+
+	main_logger.Info(fmt.Sprintf("started %d downloaders", downloader_started))
+
+	if downloader_started == 0 {
+		os.Exit(0)
+	}
+}
+
+func startUploaders(master_config config.MasterConfig) {
+
+	uploader_started := 0
+	for _, uploader_config := range master_config.Uploaders {
+		if uploader_config.Enabled {
+			uploader.NewOneTimeUploader(uploader_config, master_config.General.TempFolder)
+			uploader_started++
+		}
+	}
+
+	main_logger.Info(fmt.Sprintf("started %d uploaders", uploader_started))
+
+	if uploader_started == 0 {
+		os.Exit(0)
+	}
+}
+
+func startSyncers(master_config config.MasterConfig) {
+
+	syncer_started := 0
+	for _, syncer_config := range master_config.Syncers {
+		if syncer_config.Enabled {
+			syncer.NewOneTimeSyncer(syncer_config, master_config.General.TempFolder)
+			syncer_started++
+		}
+	}
+
+	main_logger.Info(fmt.Sprintf("started %d syncers", syncer_started))
+
+	if syncer_started == 0 {
+		os.Exit(0)
+	}
+}
+
+func startStreamers(master_config config.MasterConfig) {
+
+	streamer_started := 0
+	for _, streamer_config := range master_config.Streamers {
+		if streamer_config.Enabled {
+			streamer.NewOneTimeStreamer(streamer_config)
+			streamer_started++
+		}
+	}
+
+	main_logger.Info(fmt.Sprintf("started %d streamers", streamer_started))
+
+	if streamer_started == 0 {
+		os.Exit(0)
+	}
+}
+
+// --------------------------
+
+func startDownloadersService(master_config config.MasterConfig) {
+
+	downloader_started := 0
+	for _, downloader_config := range master_config.Downloaders {
+		if downloader_config.Enabled {
 			downloader.NewDownloader(downloader_config, master_config.General.TempFolder)
 			downloader_started++
 		}
@@ -34,7 +105,7 @@ func startDownloaders(master_config config.MasterConfig) {
 	main_logger.Info(fmt.Sprintf("started %d downloaders", downloader_started))
 }
 
-func startUploaders(master_config config.MasterConfig) {
+func startUploadersService(master_config config.MasterConfig) {
 
 	uploader_started := 0
 	for _, uploader_config := range master_config.Uploaders {
@@ -47,7 +118,7 @@ func startUploaders(master_config config.MasterConfig) {
 	main_logger.Info(fmt.Sprintf("started %d uploaders", uploader_started))
 }
 
-func startSyncers(master_config config.MasterConfig) {
+func startSyncersService(master_config config.MasterConfig) {
 
 	syncer_started := 0
 	for _, syncer_config := range master_config.Syncers {
@@ -60,7 +131,7 @@ func startSyncers(master_config config.MasterConfig) {
 	main_logger.Info(fmt.Sprintf("started %d syncers", syncer_started))
 }
 
-func startStreamers(master_config config.MasterConfig) {
+func startStreamersService(master_config config.MasterConfig) {
 
 	streamer_started := 0
 	for _, streamer_config := range master_config.Streamers {
@@ -71,6 +142,14 @@ func startStreamers(master_config config.MasterConfig) {
 	}
 
 	main_logger.Info(fmt.Sprintf("started %d streamers", streamer_started))
+}
+
+func startServices(master_config config.MasterConfig) {
+	go startDownloadersService(master_config)
+	go startUploadersService(master_config)
+	go startSyncersService(master_config)
+	go startStreamersService(master_config)
+	<-make(chan struct{})
 }
 
 // --------------------------
@@ -97,14 +176,49 @@ func init() {
 
 // --------------------------
 
+func printUsage() {
+	main_logger.Info(fmt.Sprintf("Usage:"))
+	main_logger.Info(fmt.Sprintf(""))
+	main_logger.Info(fmt.Sprintf("  ugoku <command>"))
+	main_logger.Info(fmt.Sprintf(""))
+	main_logger.Info(fmt.Sprintf("command can be upload, download, sync, stream, serve"))
+	main_logger.Info(fmt.Sprintf(""))
+	main_logger.Info(fmt.Sprintf("Example:"))
+	main_logger.Info(fmt.Sprintf(""))
+	main_logger.Info(fmt.Sprintf("  ugoku sync"))
+}
+
 func main() {
 
-	main_logger.Info(fmt.Sprintf("ugoku version %s", VERSION))
+	main_logger.Info(fmt.Sprintf("ugoku-cli version %s", VERSION))
 
-	go startDownloaders(master_config)
-	go startUploaders(master_config)
-	go startSyncers(master_config)
-	go startStreamers(master_config)
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(0)
+	}
 
-	<-make(chan struct{})
+	cmd := strings.ToLower(os.Args[1])
+
+	switch cmd {
+	case "upload":
+		go startUploaders(master_config)
+		break
+	case "download":
+		go startDownloaders(master_config)
+		break
+	case "sync":
+		go startSyncers(master_config)
+		break
+	case "stream":
+		go startStreamers(master_config)
+		break
+	case "serve":
+		startServices(master_config)
+		break
+	default:
+		main_logger.Error("Missing or unknown command")
+		printUsage()
+		os.Exit(0)
+	}
+
 }
