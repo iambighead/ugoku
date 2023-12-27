3006af677f3e1f641c116853266d9410809e1a58
iambighead <iambighead168@gmail.com>
2023-12-27 09:36:44 +0800

    mvoe sleepytime to internal

--- internal/sleepytime/sleepytime.go
+++ internal/sleepytime/sleepytime.go
@@ -1,18 +1,18 @@
-package downloader
+package sleepytime
 
-type sleepytime struct {
+type Sleepytime struct {
 	next_sleep int
 	max_sleep  int
 	half_sleep int
 }
 
-func (sleeper *sleepytime) reset(initial_time int, max_time int) {
+func (sleeper *Sleepytime) Reset(initial_time int, max_time int) {
 	sleeper.next_sleep = initial_time
 	sleeper.max_sleep = max_time
 	sleeper.half_sleep = max_time / 2
 }
 
-func (sleeper *sleepytime) getNextSleep() int {
+func (sleeper *Sleepytime) GetNextSleep() int {
 	if sleeper.next_sleep < sleeper.half_sleep {
 		sleeper.next_sleep = sleeper.next_sleep * 2
 	} else {
